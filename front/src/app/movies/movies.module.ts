c0aee32f18a4e40c7984411251518e23fca13be1
David Moreau <david.moreau@tcmlabs.fr>
2023-01-16 14:35:10 +0100

    feat: add details component skeleton

--- front/src/app/movies/movies.module.ts
+++ front/src/app/movies/movies.module.ts
@@ -6,9 +6,10 @@ import { SearchComponent } from './list/search/search.component';
 import { FormsModule } from '@angular/forms';
 import { MoviesListService } from './list/movies-list.service';
 import { SearchService } from './list/search/search.service';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [ListComponent, SearchComponent],
+  declarations: [ListComponent, SearchComponent, DetailsComponent],
   providers: [MoviesListService, SearchService],
   imports: [CommonModule, MoviesRoutingModule, FormsModule],
 })
