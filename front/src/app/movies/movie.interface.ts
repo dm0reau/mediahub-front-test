1dd0c06150112fcc9b91eab6cf67bbe6e7e1d69c
David Moreau <david.moreau@tcmlabs.fr>
2023-01-16 11:48:10 +0100

    add movie service & interface

---
+++ front/src/app/movies/movie.interface.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+  id: number;
+  title: string;
+  usGross: number;
+  usDvdSales: number;
+  worldwideGross?: number;
+  productionBudget?: number;
+  releaseDate?: string;
+  distributor?: string;
+  imdbRating?: number;
+  imdbVotes?: number;
+  majorGenre?: string;
+  director?: string;
+  rottenTomatoesRating?: string;
+}
--- front/src/app/movies/movies.module.ts
+++ front/src/app/movies/movies.module.ts
@@ -4,9 +4,11 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { ListComponent } from './list/list.component';
 import { SearchComponent } from './list/search/search.component';
 import { FormsModule } from '@angular/forms';
+import { MoviesService } from './movies.service';
 
 @NgModule({
   declarations: [ListComponent, SearchComponent],
+  providers: [MoviesService],
   imports: [CommonModule, MoviesRoutingModule, FormsModule],
 })
 export class MoviesModule {}
