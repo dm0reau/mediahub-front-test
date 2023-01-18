b69d075da9d8ff4d730cc646ad035aa8c66d36b7
David Moreau <david.moreau@tcmlabs.fr>
2023-01-18 18:54:56 +0100

    feat: inject http auth gateway

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,18 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { InMemoryAuthGateway } from '../domain/auth/adapters/in-memory-auth.gateway';
 import { NavComponent } from './nav/nav.component';
 import { LocalStorageAuthTokenRepository } from '../domain/auth/adapters/local-storage-auth-token.repository';
+import { HttpAuthGateway } from '../domain/auth/adapters/http-auth.gateway';
+import { AngularMediaHubHttpClient } from '../infra/adapters/angular-media-hub-http-client';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
     {
       provide: 'AuthGateway',
-      useFactory: () =>
-        new InMemoryAuthGateway(new LocalStorageAuthTokenRepository()),
+      useFactory: (httpClient: HttpClient) => {
+        const angularMediaHubHttpClient = new AngularMediaHubHttpClient(
+          httpClient
+        );
+        return new HttpAuthGateway(
+          new LocalStorageAuthTokenRepository(),
+          angularMediaHubHttpClient
+        );
+      },
+      deps: [HttpClient],
     },
   ],
   bootstrap: [AppComponent],
