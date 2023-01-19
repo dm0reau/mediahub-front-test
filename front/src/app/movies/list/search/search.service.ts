651f250db4bd44e6aa6e860845d0a40879930546
David Moreau <david.moreau@tcmlabs.fr>
2023-01-19 14:40:01 +0100

    feat: add sampleTime for search

--- front/src/app/movies/list/search/search.service.ts
+++ front/src/app/movies/list/search/search.service.ts
@@ -1,30 +1,40 @@
 import { Inject, Injectable } from '@angular/core';
 import { MoviesListStateService } from '../movies-list-state.service';
 import { MovieGateway } from '../../../../domain/movie/ports/movie.gateway';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, sampleTime, Subject } from 'rxjs';
 import { MovieSortAttribute } from '../../../../domain/movie/models/movie-sort-attribute';
+
+const THROTTLE_TIME_IN_SECONDS = 1000;
 
 @Injectable()
 export class SearchService {
+  private searchQueries$ = new Subject<string>();
+
   constructor(
     private readonly moviesListStateService: MoviesListStateService,
     @Inject('MoviesGateway')
     private readonly moviesGateway: MovieGateway
-  ) {}
+  ) {
+    this.searchQueries$
+      .pipe(sampleTime(THROTTLE_TIME_IN_SECONDS))
+      .subscribe((query) => {
+        this.moviesListStateService.setState({ searchQuery: query });
+
+        if (query.length === 0) {
+          this.moviesListStateService.setState({ moviesList: [] });
+          return;
+        }
+
+        this.moviesGateway
+          .searchMovies(query)
+          .subscribe((movies) =>
+            this.moviesListStateService.setState({ moviesList: movies })
+          );
+      });
+  }
 
   search(query: string) {
-    this.moviesListStateService.setState({ searchQuery: query });
-
-    if (query.length === 0) {
-      this.moviesListStateService.setState({ moviesList: [] });
-      return;
-    }
-
-    this.moviesGateway
-      .searchMovies(query)
-      .subscribe((movies) =>
-        this.moviesListStateService.setState({ moviesList: movies })
-      );
+    this.searchQueries$.next(query);
   }
 
   async sort(sortBy: MovieSortAttribute) {
