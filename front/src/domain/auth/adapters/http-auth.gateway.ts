8c8827bae7a1673dd5afd20ab46ab1450c1a6dac
David Moreau <david.moreau@tcmlabs.fr>
2023-01-19 16:04:23 +0100

    fix: use isLoggedIn Subject in http implementation

--- front/src/domain/auth/adapters/http-auth.gateway.ts
+++ front/src/domain/auth/adapters/http-auth.gateway.ts
@@ -1,24 +1,29 @@
 import { AuthGateway } from '../ports/auth.gateway';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of, tap } from 'rxjs';
 import { AuthTokenRepository } from '../ports/auth-token.repository';
 import { MhHttpClient } from '../../../infra/ports/mh-http-client';
 
 export class HttpAuthGateway implements AuthGateway {
+  private isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
   constructor(
     private readonly authTokenRepository: AuthTokenRepository,
     private readonly mhHttpClient: MhHttpClient
-  ) {}
+  ) {
+    this.isLoggedIn$.next(this.authTokenRepository.hasToken());
+  }
 
   invalidate(): Observable<null> {
     return of(null).pipe(
       tap(() => {
         this.authTokenRepository.remove();
+        this.isLoggedIn$.next(false);
       })
     );
   }
 
   isLoggedIn(): Observable<boolean> {
-    return of(this.authTokenRepository.hasToken());
+    return this.isLoggedIn$.asObservable();
   }
 
   validate(userName: string, password: string): Observable<boolean> {
@@ -37,6 +42,7 @@ export class HttpAuthGateway implements AuthGateway {
           }
           return false;
         }),
+        tap(() => this.isLoggedIn$.next(true)),
         catchError(() => of(false))
       );
   }
